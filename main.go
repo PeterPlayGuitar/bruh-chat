146a570a9327433892923605c2c4ea4fe718968c
PeterZelinskiy <zelinskiypk@sushivesla.team>
2024-06-26 17:02:17 +0300

    completed

---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"encoding/json"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Message struct {
+	ID     string `bson:"_id,omitempty" json:"_id,omitempty"`
+	Sender string `bson:"sender" json:"sender"`
+	Value  string `bson:"value" json:"value"`
+}
+
+var mongoClient *mongo.Client
+
+type Env struct {
+	mongodbPort string
+}
+
+var envDev = Env{
+	mongodbPort: "27010",
+}
+
+var envProd = Env{
+	mongodbPort: "27017",
+}
+
+var profile = os.Args[1]
+
+func main() {
+	var env Env
+	if profile == "local" {
+		env = envDev
+	} else {
+		env = envProd
+	}
+
+	// Set MongoDB client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:" + env.mongodbPort)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mongoClient = client
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	port := "65000"
+	http.Handle("/chat/", http.StripPrefix("/chat/", http.FileServer(http.Dir("./public"))))
+	http.HandleFunc("/api/messages", messagesHandler)
+	//certFile := "cert.pem"
+	//keyFile := "key.pem"
+	log.Println("Starting server on https://localhost:" + port)
+	//err = http.ListenAndServeTLS(":"+port, certFile, keyFile, nil)
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
+
+func messagesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		getMessagesApi(w, r)
+		return
+	} else if r.Method == http.MethodPost {
+		addMessageApi(w, r)
+		return
+	}
+	http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+}
+
+func getMessagesApi(w http.ResponseWriter, _ *http.Request) {
+
+	messages := findAll()
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(messages); err != nil {
+		http.Error(w, "Failed to encode messages", http.StatusInternalServerError)
+	}
+}
+
+func addMessageApi(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	var msg Message
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	addMessage(msg)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func addMessage(message Message) *mongo.InsertOneResult {
+	// Specify the database and collection
+	collection := mongoClient.Database("bruhChat").Collection("messages")
+
+	// Insert the new user into the collection
+	insertResult, err := collection.InsertOne(context.TODO(), message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return insertResult
+}
+
+func findAll() []Message {
+	// Specify the database and collection
+	collection := mongoClient.Database("bruhChat").Collection("messages")
+
+	findOptions := options.Find()
+	findOptions.SetLimit(3)
+	findOptions.SetSort(bson.D{{"_id", -1}}) // Sort by _id in descending order
+
+	cursor, err := collection.Find(context.TODO(), bson.D{}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the cursor once finished
+	defer cursor.Close(context.TODO())
+
+	// Iterate through the cursor and decode each document
+	var results []Message
+	for cursor.Next(context.TODO()) {
+		var message Message
+		err := cursor.Decode(&message)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, message)
+	}
+
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return results
+}
---
+++ public/logic.js
@@ -0,0 +1,95 @@
+url = "http://localhost:65000";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const textarea = document.getElementById("input");
+  const senderInput = document.getElementById("senderInput");
+  const messagesBox = document.getElementById("messages");
+
+  everyPeriod(1000, () => {
+    update(messagesBox);
+  });
+
+  textarea.addEventListener("input", function (e) {
+    this.style.height = "auto";
+    if (this.scrollHeight < 200) this.style.height = this.scrollHeight + "px";
+    else this.style.height = "200px";
+  });
+
+  textarea.addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.keyCode === 13) {
+      event.preventDefault();
+
+      if (textarea.value === "") return;
+
+      sendMessage(
+        {
+          value: textarea.value,
+          sender: senderInput.value === "" ? "anonim" : senderInput.value,
+        },
+        () => {
+          textarea.value = "";
+          update(messagesBox);
+        }
+      );
+    }
+  });
+
+  update(messagesBox);
+});
+
+function everyPeriod(time, func) {
+  setTimeout(() => {
+    func();
+    everyPeriod(time, func);
+  }, time);
+}
+
+function update(messagesBox) {
+  getMessages((response) => {
+    messagesBox.innerHTML = "";
+    if (response === null) return;
+    response.reverse().forEach((element) => {
+      var msgDiv = document.createElement("div");
+      var valueDiv = document.createElement("div");
+      var senderDiv = document.createElement("div");
+
+      // Add content and attributes to the div
+      valueDiv.textContent = element.value;
+      senderDiv.textContent = element.sender;
+      msgDiv.classList.add("message");
+      senderDiv.classList.add("sender");
+      valueDiv.classList.add("value");
+      msgDiv.appendChild(senderDiv);
+      msgDiv.appendChild(valueDiv);
+
+      // Append the new div to the body (or any other parent element)
+      messagesBox.appendChild(msgDiv);
+    });
+    messagesBox.scrollTop = messagesBox.scrollHeight;
+  });
+}
+
+function getMessages(func) {
+  fetch(url + "/api/messages", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      // Add other headers as needed
+    },
+  }).then((response) => {
+    response.json().then((data) => func(data));
+  });
+}
+
+function sendMessage(msg, success) {
+  fetch(url + "/api/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // Add other headers as needed
+    },
+    body: JSON.stringify(msg),
+  }).then(() => {
+    success();
+  });
+}
