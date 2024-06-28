82723cf0e54c9639792b7f7c2c3158ed5501a8e0
PeterZelinskiy <zelinskiypk@sushivesla.team>
2024-06-28 18:14:05 +0300

    url

--- public/logic.js
+++ public/logic.js
@@ -1,4 +1,4 @@
-url = "http://rabit1998g.fvds.ru:65000";
+url = "http://rabit1998g.fvds.ru/bruh-chat";
 
 document.addEventListener("DOMContentLoaded", () => {
   const textarea = document.getElementById("input");
